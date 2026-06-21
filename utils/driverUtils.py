3942c2e73ebab46068e262e27befd2025185bec5
aishanand0212-cmyk <aishanand0212@gmail.com>
2026-06-22 00:04:09 +0530

    changes

--- utils/driverUtils.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-def LaunchBrowser():
-    driver=webdriver.Chrome()
-    driver.implicitly_wait(10)
-    return driver
-
-def closebrowser(driver):
-    driver.quit()
